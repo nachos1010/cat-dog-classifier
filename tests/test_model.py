396ad91b18e7553546de2f4368b8ed1f5daa6184
@Argon179 <asanov2003@inbox.ru>
2025-10-24 18:35:59 +0300

    Fixing bugs

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from model import CatDogClassifier
-import os
+
 def test_model_loads():
     classifier = CatDogClassifier()
     assert classifier.model is not None
